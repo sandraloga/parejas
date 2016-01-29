112568e5f9458065bd3e544b5a1011af3bd19d9a
Sandra <72sandraloga@gmail.com>
2016-01-29 13:36:14 +0100

    Creado un ArrayList con los nombres de los alumno

---
+++ parejas.java
@@ -0,0 +1,60 @@
+import java.util.ArrayList;
+/**
+ * Write a description of class parejas here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class parejas
+{
+    // instance variables - replace the example below with your own
+    private ArrayList<String> alumnos;
+    private String nombre;
+    private int numero;
+
+    /**
+     * Constructor for objects of class parejas
+     */
+    public datosAlumno(String nombre, int numero)
+    {
+        setDatosAlumno(nombre,numero);
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public void nombres Alumnos()
+    {
+        alumnos.add("Adeva Tranche, Adrián");
+        alumnos.add("Aller González, Francisco Javier");
+        alumnos-add("Álvarez Prieto, Noelia");
+        alumnos-add("Barrionuevo Pérez, Manuel");
+        alumnos.add("Bóveda del Río, José Antonio");
+        alumnos.add("de la Viuda Crespo, Ignacio");
+        alumnos.add("Encina Maestro, David");
+        alumnos.add("Felix Nañez, Kevin");
+        alumnos.add("Fernández Diez, Daniel");
+        alumnos.add("Fernández González, Javier");
+        alumnos.add("Fernández Rincón, Jesús");
+        alumnos.dd("Fuentes Álvarez, Francisco José");
+        alumnos.add("García Juárez, David");
+        alumnos.add("García Serrano, Rubén");
+        alumnos.add("González Álvarez, David");
+        alumnos.add("López Beltrán, Víctor");
+        alumnnos.add("Lora García, Sandra");
+        alumnos.add("Melcón Diez, Víctor");
+        alumnos.add("Menéndez Pouso, Francisco José");
+        alumnos.add("Pérez Bayón, Álvaro");
+        alumnos.add("Pradera San José, Daniel");
+        alumnos.add("Prieto Mediavilla, Gabriel");
+        alumnos.add("Robles González, Miguel Ángel")
+        alumnos.add("Sánchez Manzano, Adán");
+        alumnos.add("Serrano García, Verónica");
+        alumnos.add("Tascón González, Anibal");
+        
+        
+    }
+}
